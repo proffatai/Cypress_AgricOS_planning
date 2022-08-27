9185acfce72259162a161275b9c0a17ccfcef212
proffatai <proffatai@gmail.com>
2022-08-27 21:31:05 +0100

    Completed BDD for Loginpage

---
+++ cypress/integration/Login Page.feature
@@ -0,0 +1,32 @@
+Feature: Login page
+
+    Only user with valid login credentials should be able to login
+
+    Scenario: User with valid username and valid password
+        Given User is on the homepage of the planning module
+        When User enters correct username
+        And  User enters correct password
+        And User clicks the login button
+        Then User should be taken to the dashboard
+        
+    Scenario: User with invalid username and an valid password
+        When User is on the homepage of the planning module
+        When User enters incorrect username
+        And  User enters correct password
+        And User clicks the login button
+        Then User should not be taken to the dashboard
+
+        
+    Scenario: User with invalid username and an invalid password
+        When User is on the homepage of the planning module
+        When User enters incorrect username
+        And  User enters incorrect password
+        And User clicks the login button
+        Then User should not be taken to the dashboard
+
+    Scenario: User with valid username and an invalid password
+        When User is on the homepage of the planning module
+        When User enters correct username
+        And  User enters incorrect password
+        And User clicks the login button
+        Then User should not be taken to the dashboard
---
+++ cypress/integration/Login Page/Login Page.js
@@ -0,0 +1,100 @@
+import {Given, When, And, Then} from 'cypress-cucumber-preprocessor/steps'
+/// <reference types ="cypress">
+
+let credentials;
+
+// Successful Login
+Given('User is on the homepage of the planning module',()=>{
+        cy.visit('/')
+        cy.url().should('equal','http://planning.uat.agric-os.com/')
+
+        // importing the stored username and password     
+    cy.fixture('Login_credentials').then( (data) =>{
+         credentials=data
+            })
+    })
+
+When('User enters correct username',()=>{
+        cy.get('#username').type(credentials.username)
+    })
+    
+And('User enters correct password',()=>{
+        cy.get('#password').type(credentials.password)
+    })
+
+And('User clicks the login button',()=>{
+        cy.get('.MuiButton-root').click()
+    })
+    
+Then('User should be taken to the dashboard',()=>{
+        cy.get('.MuiToolbar-root > .MuiTypography-root')
+        .should('contain','Dashboard').and('be.visible') // verifying that the first page is the dashboard
+    })
+
+// Unsuccessful Login
+Given('User is on the homepage of the planning module',()=>{
+        cy.visit('/')
+        cy.url().should('equal','http://planning.uat.agric-os.com/')
+    })
+
+When('User enters incorrect username',()=>{
+        cy.get('#username').type(credentials.inUsername)
+    })
+    
+And('User enters correct password',()=>{
+        cy.get('#password').type(credentials.password)
+    })
+
+And('User clicks the login button',()=>{
+        cy.get('.MuiButton-root').click()
+    })
+    
+Then('User should not be taken to the dashboard',()=>{
+    cy.get('[style="color: red; text-align: center;"]').should('have.text','User ' + credentials.inUsername+' not found')
+    })
+
+// Unsuccessful Login
+Given('User is on the homepage of the planning module',()=>{
+        cy.visit('/')
+        cy.url().should('equal','http://planning.uat.agric-os.com/')
+    })
+
+When('User enters incorrect username',()=>{
+        cy.get('#username').type(credentials.inUsername)
+    })
+    
+And('User enters incorrect password',()=>{
+        cy.get('#password').type(credentials.inPassword)
+    })
+
+And('User clicks the login button',()=>{
+        cy.get('.MuiButton-root').click()
+    })
+    
+Then('User should not be taken to the dashboard',()=>{
+    cy.get('[style="color: red; text-align: center;"]').should('have.text','User ' + credentials.inUsername+' not found')
+    })    
+    
+ // Unsuccessful Login
+Given('User is on the homepage of the planning module',()=>{
+        cy.visit('/')
+        cy.url().should('equal','http://planning.uat.agric-os.com/')
+    })
+
+When('User enters correct username',()=>{
+        cy.get('#username').type(credentials.username)
+    })
+    
+And('User enters incorrect password',()=>{
+        cy.get('#password').type(credentials.inPassword)
+    })
+
+And('User clicks the login button',()=>{
+        cy.get('.MuiButton-root').click()
+    })
+    
+Then('User should not be taken to the dashboard',()=>{
+    cy.get('[style="color: red; text-align: center;"]').should('have.text','Your password not matching')
+    })
+
+   
