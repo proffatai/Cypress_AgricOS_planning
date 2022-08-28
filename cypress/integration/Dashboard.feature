ff4124f8474e1d3db745d859dc714dd92540e309
proffatai <proffatai@gmail.com>
2022-08-28 09:19:57 +0100

    Completed step definition for Dashboard

--- cypress/integration/Dashboard.feature
+++ cypress/integration/Dashboard.feature
@@ -1,3 +1,28 @@
 Feature: Dashboard screen
 
-    User has successfully logged in to the application
+    User has successfully logged in to the application and can access the application
+
+
+    Scenario: User with valid username and valid password
+        Given User is on the homepage of the planning module
+        When User enters correct username
+        And  User enters correct password
+        And User clicks the login button
+        Then User should be taken to the dashboard
+
+    Scenario: Verify all the Menus on the dashboard are present
+        Given User has successfully logged in to the application
+        When User is on the Dashboard
+        Then Application should display all the menus
+   
+    Scenario: User wants to access some functionalities on the dashboard
+        Given  User has successfully logged in to the application
+        When User wants to verify if some components are present
+        Then Application should display the components
+
+
+    Scenario:  User should be able to sign out of the application
+        Given User has successfully logged in to the application
+        When User access the log out button
+        And User clicks on the log out button
+        Then User is taken out of the application
--- cypress/integration/Dashboard/Dashboard.js
+++ cypress/integration/Dashboard/Dashboard.js
@@ -1,49 +1,113 @@
-describe('Homescreen of planning OS', () => {
-  it('Dashboard Menu', () => {
+import {
+  Given,
+  When,
+  And,
+  Then,
+  Before,
+} from "cypress-cucumber-preprocessor/steps";
+/// <reference types ="cypress">
 
-    const menus =[
-      "Dashboard",
-      "General Settings",
-      "Management",
-      "Member Settings",
-      "Lock Input",
-      "Goal Settings",
-      "Farm Input",
-      "Credit Sustainability",
-      "Location planning",
-      "Personnel planning",
-      "Logistics",
-      "Assets",
-      "App Support"
-    ]
-    cy.visit('/')
-    cy.get('#username').type('lagos.admin')
-    cy.get('#password').type('1234')
-    cy.get('.MuiButton-root').click()
-    cy.get('.MuiToolbar-root > .MuiTypography-root')
-      .should('contain','Dashboard').and('be.visible') // verifying that the first page is the dashboard
-   
-      //Checking if all the menus are present
-      let i=0
-      while(i<menus.length){
-        cy.get('.MuiList-padding').should('contain',menus[i]).and('be.visible')
-        i++
-      }
+let credentials;
+let url = "http://planning.uat.agric-os.com/";
+let menus;
+//for cucumber framework, we use the Before() hook and not the beforeEach or before
+Before(() => {
+  // importing the stored username and password
+  cy.fixture("Login_credentials").then((data) => {
+    credentials = data;
+  });
+  menus = [
+    "Dashboard",
+    "General Settings",
+    "Management",
+    "Member Settings",
+    "Lock Input",
+    "Goal Settings",
+    "Farm Input",
+    "Credit Sustainability",
+    "Location planning",
+    "Personnel planning",
+    "Logistics",
+    "Assets",
+    "App Support",
+  ];
+});
 
-      //verifying that the hub id is present
-      cy.get('.css-6tke00-MuiTypography-root').should('have.text', 'Hub: Lagos')
-      //verifying that the Overall Status is present
-      cy.get('.css-0 > .MuiTypography-root').should('contain', 'Overall Status')
-      
-      //checking out for the logout button
-      cy.contains('Logout').should('be.enabled').and('be.visible')
-     
-     //Logging out of the app
-      cy.contains('Logout').click({force:true})
-     
-      //Affirming that the appp has signed out successfully by checking the url of the new page
-      let url ='http://planning.uat.agric-os.com/'
-      cy.url().should('equal', url) // alternative cy.url().should('eq',url)
+// Step definition for a successful Login
+Given("User is on the homepage of the planning module", () => {
+  cy.visit("/");
+  cy.url().should("equal", url);
+});
+When("User enters correct username", () => {
+  cy.get("#username").type(credentials.username);
+});
 
-  })
-})
+And("User enters correct password", () => {
+  cy.get("#password").type(credentials.password);
+});
+
+And("User clicks the login button", () => {
+  cy.get(".MuiButton-root").click();
+});
+
+Then("User should be taken to the dashboard", () => {
+  cy.get(".MuiToolbar-root > .MuiTypography-root")
+    .should("contain", "Dashboard")
+    .and("be.visible"); // verifying that the first page is the dashboard
+});
+
+//Step definition to verify all the Menus on the dashboard are present
+Given("User has successfully logged in to the application", () => {
+  cy.visit("/");
+  cy.get("#username").type(credentials.username);
+  cy.get("#password").type(credentials.password);
+  cy.get(".MuiButton-root").click();
+
+  cy.get(".MuiToolbar-root > .MuiTypography-root")
+    .should("contain", "Dashboard")
+    .and("be.visible"); // verifying that the first page is the dashboard
+});
+
+When("User is on the Dashboard", () => {
+  cy.url().should("equal", "http://planning.uat.agric-os.com/dashboard");
+  cy.get(".MuiToolbar-root > .MuiTypography-root")
+    .should("contain", "Dashboard")
+    .and("be.visible"); // verifying that the first page is the dashboard
+});
+Then("Application should display all the menus", () => {
+  //Checking if all the menus are present
+  let i = 0;
+  while (i < menus.length) {
+    cy.get(".MuiList-padding").should("contain", menus[i]).and("be.visible");
+    i++;
+  }
+});
+//Step definition for User wants to access some functionalities on the dashboard
+Given("User has successfully logged in to the application", () => {});
+When("User wants to verify if some components are present", () => {
+    //Accessing some components on the dashboard
+    cy.get(".css-6tke00-MuiTypography-root")
+    cy.get(".css-0 > .MuiTypography-root")
+});
+Then("Application should display the components", () => {
+  //verifying that the hub id is present
+  cy.get(".css-6tke00-MuiTypography-root").should("have.text", "Hub: Lagos");
+  //verifying that the Overall Status is present
+  cy.get(".css-0 > .MuiTypography-root").should("contain", "Overall Status");
+});
+//step definition for logout
+Given("User has successfully logged in to the application", () => {});
+When("User access the log out button", () => {
+  //checking out for the logout button
+  cy.contains("Logout").should("be.enabled").and("be.visible");
+});
+
+And("User clicks on the log out button", () => {
+  //Logging out of the app
+  cy.contains("Logout").click({ force: true });
+});
+
+Then("User is taken out of the application", () => {
+  //Affirming that the appp has signed out successfully by checking the url of the new page
+  cy.url().should("equal", url); // alternative cy.url().should('eq',url)
+});
