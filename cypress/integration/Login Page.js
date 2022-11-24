473b0a28fdc132c7278c78210bc839c264268a02
proffatai <proffatai@gmail.com>
2022-11-24 05:08:10 +0100

    refactoring login page

--- cypress/integration/Login Page.js
+++ cypress/integration/Login Page.js
@@ -7,40 +7,31 @@ describe('Planning Login Screen', ()=>{
         cy.fixture('Login_credentials').then( (data) =>{
             credentials=data
     })
-
     })
 
 
     it('Valid username and valid password',()=>{
-   
-        cy.get('#username').type(credentials.username)
-        cy.get('#password').type(credentials.password)
-        cy.get('.MuiButton-root').click()
+        
+        cy.login(credentials.username,credentials.password)
         cy.get('.MuiToolbar-root > .MuiTypography-root')
         .should('contain','Dashboard').and('be.visible') // verifying that the first page is the dashboard
     })
 
     it('Invalid username and invalid password',()=>{
        
-        cy.get('#username').type(credentials.inUsername)
-        cy.get('#password').type(credentials.inPassword)
-        cy.get('.MuiButton-root').click()
+        cy.login(credentials.inUsername, credentials.inPassword)
         cy.get('[style="color: red; text-align: center;"]').should('have.text','User ' + credentials.inUsername+' not found')
     })
 
     it('Invalid username and valid password',()=>{
         
-        cy.get('#username').type(credentials.inUsername)
-        cy.get('#password').type(credentials.password)
-        cy.get('.MuiButton-root').click()
+        cy.login(credentials.inUsername, credentials.password)
         cy.get('[style="color: red; text-align: center;"]').should('have.text','User ' + credentials.inUsername+' not found')
     })
 
     it('Valid username and invalid password',()=>{
         
-        cy.get('#username').type(credentials.username)
-        cy.get('#password').type(credentials.inPassword)
-        cy.get('.MuiButton-root').click()
+        cy.login(credentials.username, credentials.inPassword)
         cy.get('[style="color: red; text-align: center;"]').should('have.text','Your password not matching')
     })
 })
